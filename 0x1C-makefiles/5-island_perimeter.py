6d215d5c7de6948838b179d4e176f3fa72d96950
WilsonTenk <tenkorangwilson9@gmail.com>
2023-11-22 16:32:05 -0800

    makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+    Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+        Returns the perimeter of the island described in grid
+    """
+    perimeter = 0
+
+    grid_height = len(grid)
+    grid_width = len(grid[0]) if grid_height else 0
+
+    for i in range(grid_height):
+        for j in range(grid_width):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if j < grid_width - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+                if i < grid_height - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
